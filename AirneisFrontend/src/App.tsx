f13190a7b271fa6a8e50919f063078572a07bb41
NeverslowLVII <sofiane57800ben@gmail.com>
2023-11-16 23:47:21 +0100

    first commit

---
+++ AirneisFrontend/src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <header>Airneis</header>
+      </div>
+      <main></main>
+      <footer>Tous droits réservés</footer>
+    </>
+  )
+}
+
+export default App
