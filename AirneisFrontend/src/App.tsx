bac651c4cab468e11af5b28b566a502552e6e4ac
NeverslowLVII <sofiane57800ben@gmail.com>
2023-11-17 20:04:36 +0100

    bootstrap

--- AirneisFrontend/src/App.tsx
+++ AirneisFrontend/src/App.tsx
@@ -1,28 +1,46 @@
-import './App.css'
+import { Container, Navbar, NavbarBrand, Nav, Row, Col } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
   return (
     <>
-      <div>
-        <header>Airneis</header>
+      <div className="d-flex flex-column vh-100">
+        <header>
+          <Navbar bg="dark" variant="dark" expand="lg">
+            <Container>
+              <NavbarBrand>Airneis</NavbarBrand>
+            </Container>
+            <Nav>
+              <a href="/cart" className="nav-link">
+                Panier
+              </a>
+              <a href="/signin" className="nav-link">
+                Connexion
+              </a>
+            </Nav>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Row>
+              {sampleProducts.map((product) => (
+                <Col key={product.slug} sm={6} md={4} lg={3}>
+                  <img
+                    src={product.image}
+                    alt={product.name}
+                    className="product-image"
+                  />
+                  <h2>{product.name}</h2>
+                  <p>${product.price}</p>
+                </Col>
+              ))}
+            </Row>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">Tous droits réservés</div>
+        </footer>
       </div>
-      <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>${product.price}</p>
-            </li>
-          ))}
-        </ul>
-      </main>
-      <footer>Tous droits réservés</footer>
     </>
   )
 }
