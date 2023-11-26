461d1268d0102cdf02a4fc42014b4b800042ccbb
NeverslowLVII <sofiane57800ben@gmail.com>
2023-11-26 23:06:09 +0100

    fixed searchbar for build

--- AirneisFrontend/src/components/SearchBar.tsx
+++ AirneisFrontend/src/components/SearchBar.tsx
@@ -6,7 +6,7 @@ const SearchBar = () => {
   const [search, setSearch] = useState('')
   const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
     navigate(`/search?query=${search}`)
   }
